07ecad7cd928c037039fa5295f86c212154aacd3
Kunal Kishore <67117612+kunalk4258@users.noreply.github.com>
2023-04-19 23:48:43 +0530

    Update QR.py

--- QR.py
+++ QR.py
@@ -1,7 +1,7 @@
 import qrcode
-
-data = 'Hey this is my QR'
-
-img = qrcode.make(data)
-
-img.save('C:\Users\KIIT\OneDrive\Documents\Programming\Python\Projects\myqrcode.png')
+qr = qrcode.QRCode(version=2, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+data = "Hello, World!"
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill_color="red", back_color="black")
+img.save("qr_code.png")
